bbe8d2e4aa196fa9fce52ef32d0e7136527419b4
sanjaysugumar2005 <sanjaysugumar2005@gmail.com>
2026-07-08 22:56:52 +0530

    loop

---
+++ looping.js
@@ -0,0 +1,60 @@
+
+
+
+
+
+// 5) Multiples of 3 and 5 
+// 6) Multiples of 3 or 5 
+// 7) Divisors of given number 
+
+
+// 1) 1   1   1   1   1
+
+
+// let i = 1;
+// let str = "";
+// while (i <= 500) {
+//     // str += 1 + ' ';
+//     // str = str + 1 + ' ';
+//     // process.stdout.write("1 ")
+//     i++;
+// }
+// console.log(str);
+
+// 2) 1   2   3   4   5
+
+
+// let i=1;
+// while(i<=5){
+//     console.log(i);
+//     i++;
+// }
+
+
+// 3) 1   3   5   7   9
+
+// let i = 1;
+// while (i <= 9) {
+//     console.log(i);
+//     // i = i + 2;
+//     i += 2;
+// }
+
+
+// 4) 3   6   9   12  15
+
+// let i=3;
+// while(i<16){
+//     console.log(i);
+//     i+=3;  
+// }
+
+
+
+
+
+// // multiples of 3 and 5
+// let noOfLines = 16;
+// for (let i = 1; i <= noOfLines; i++) {
+//     console.log(`${i * 3}    ${i * 5}`);
+// }
