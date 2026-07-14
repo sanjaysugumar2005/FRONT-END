ddb309c234b4dece37bb16b5a60e82eceeafde13
sanjaysugumar2005 <sanjaysugumar2005@gmail.com>
2026-07-14 23:45:27 +0530

    work

---
+++ crud.js
@@ -0,0 +1,31 @@
+// // CRUD
+// CREATE READ UPDATE DELETE
+
+
+// CREATE
+// let students=["arun","deva"];
+// students.push("sanjay");
+// console.log(students);
+
+
+// READ
+
+// let students=["arun","deva"];
+// students.push("sanjay");
+// console.log(students[1]);
+// console.log(students)
+
+// UPDATE
+
+// let students=["arun","deva"];
+// students[2]="sanjay";
+// console.log(students)
+
+
+// DELETE
+
+// let students = ["Sanjay", "Rahul", "Kumar"];
+
+// students.splice(1, 1);
+
+// console.log(students);
