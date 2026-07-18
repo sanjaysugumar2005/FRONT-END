d5f9f1cbc6694771293fc04dc89e607da0bbd5c0
sanjaysugumar2005 <sanjaysugumar2005@gmail.com>
2026-07-19 02:39:52 +0530

    FUNCTION

---
+++ function.js
@@ -0,0 +1,79 @@
+// function makeBiriyani(){
+//     let container = "biriyani";
+//     console.log("biriyani ready");
+//     return container;
+    
+// }
+
+// let foodContainer=makeBiriyani();
+// console.log(foodContainer);
+
+// let age=10;
+
+// if(true){
+//     let age=20;
+//     console.log(age);
+// }
+
+// console.log(age);
+
+
+
+// let age=10;
+
+// if(true){
+//     age=20;
+//     console.log(age);
+// }
+
+// console.log(age);
+
+
+// const age=10;
+
+// if(true){
+//    const age=20;
+//     console.log(age);
+// }
+
+// console.log(age);
+
+
+
+// let age=10;
+
+// if(true){
+//     age=20;
+//     console.log(age);
+//     if(true){
+//         age=30
+//         console.log(age);
+        
+//     }
+// }
+
+// console.log(age);
+
+
+// let age=null;
+
+// if(age==null){
+//     console.log("its null");
+    
+// }
+
+
+
+// let person={
+//     name:"sanjay",
+//     age:21,
+//     place:"chennai",
+// };
+
+// function parameterthis(){
+// console.log(person.name);
+
+// }
+// parameterthis()
+
+
