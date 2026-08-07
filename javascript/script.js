07d1d69e459ae947211671569c97e0b44df04a6e
sanjaysugumar2005 <sanjaysugumar2005@gmail.com>
2026-08-07 23:29:06 +0530

    san

---
+++ javascript/script.js
@@ -0,0 +1,8 @@
+   // console.log("Happy New Year")
+        // alert("hi")
+        
+        // let score=1000
+        // console.log("score is ", score)
+
+         let score=1000
+        console.log("Your bill Amount", score)
