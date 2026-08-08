7fd75081d62c9df0fe51b946271dc4787524b0fd
sanjaysugumar2005 <sanjaysugumar2005@gmail.com>
2026-08-08 17:28:08 +0530

    sanjay

--- javascript/script.js
+++ javascript/script.js
@@ -4,5 +4,60 @@
         // let score=1000
         // console.log("score is ", score)
 
-         let score=1000
-        console.log("Your bill Amount", score)
+        //  let score=1000
+        // console.log("Your bill Amount", score)
+
+      //   let a = 10;
+
+      //   a++;
+      //   console.log(a);
+
+      // let a = 10;
+
+      // a++;
+      // console.log(a);
+
+      // let a = 10;
+
+      // a--;
+      // console.log(a);
+
+      // let x = 5;
+      //    x++;
+      //    x++;
+      //    console.log(x);
+         // let x = 10;
+         //    x--;
+         //    x--;
+         //    console.log(x);
+
+         // let x = 5;
+         //    x++;
+         //    x--;
+         //    x++;
+         //    console.log(x);
+
+            // let x = 10;
+            //    x += 5;
+            //    x--;
+            //    console.log(x);
+
+            // let x = 20;
+            // //    // x--;
+            // // //    x--;
+            // //    ++x;
+            // //    console.log(x);
+
+
+            // let a = 5;
+            // let b = a++;
+            // console.log(b);
+            // console.log(a);
+            // console.log(b);
+            // console.log(b);
+
+            // document.getElementById("message")
+
+
+
+            
